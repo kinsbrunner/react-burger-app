4c07fb8013bda991b4a109e7df7444aefbfd0d87
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2018-05-25 19:22:59 -0300

    Adding Redux into the project

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
@@ -108,12 +109,9 @@ class ContactData extends Component {
             formData[formElementId] = this.state.orderForm[formElementId].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData,
-            validation: {
-                required: true
-            }
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -216,4 +214,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
